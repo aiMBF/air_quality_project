a8d9f8a1f65182290ebd2508c5ac17175acb9009
aiMBF <aissasaykou@gmail.com>
2024-11-18 23:58:57 +0100

    config connexion between Pyspark and gcs bucket

--- pyspark/data_load.py
+++ pyspark/data_load.py
@@ -8,7 +8,7 @@ load_dotenv(dotenv_path=".secrets")
 gcs_key_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
 spark = SparkSession.builder \
-    .appName("GCS Read Example") \
+    .appName("GCS Read CO2 data") \
     .config("spark.jars", "../configs/gcs-connector-hadoop3-latest.jar") \
     .config("spark.hadoop.google.cloud.auth.service.account.enable", "true") \
     .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", gcs_key_path) \
