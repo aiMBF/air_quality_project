27036e8e09fd26e9438646525266c59926c34539
aiMBF <aissasaykou@gmail.com>
2024-11-18 23:57:45 +0100

    Configure PySpark to read files from GCS bucket

--- pyspark/data_load.py
+++ pyspark/data_load.py
@@ -1,8 +1,15 @@
 from dotenv import load_dotenv
 import os
+from pyspark.sql import SparkSession
 
-# Load the .env file
+
 load_dotenv(dotenv_path=".secrets")
 
-# Access the API key
-API_KEY = os.getenv("API_KEY")
+gcs_key_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+spark = SparkSession.builder \
+    .appName("GCS Read Example") \
+    .config("spark.jars", "../configs/gcs-connector-hadoop3-latest.jar") \
+    .config("spark.hadoop.google.cloud.auth.service.account.enable", "true") \
+    .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", gcs_key_path) \
+    .getOrCreate()
