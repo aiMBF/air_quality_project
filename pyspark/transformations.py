4b11c37ca597f27d843443bf4c7ba0d76eb05e83
aiMBF <aissasaykou@gmail.com>
2024-11-19 23:49:15 +0100

    Adding transformations and KPIs calculation: annual CO2 emissions per capita and Cumulative CO2 annual emissions by country

---
+++ pyspark/transformations.py
@@ -0,0 +1,10 @@
+from pyspark.sql.window import Window
+from pyspark.sql import functions as F
+
+
+def calculate_co2_per_capita(df):
+    return df.withColumn("co2_per_capita", df["Annual CO₂ emissions"] / df["population"])
+
+def calculate_cumulative_co2(df):
+    window_spec = Window.partitionBy("iso_code").orderBy("Year")
+    return df.withColumn("cumulative_co2_emissions", F.sum("Annual CO₂ emissions").over(window_spec))
