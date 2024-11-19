6793700c4f863dbc342ab2cc6f4d7d5bfed795b9
aiMBF <aissasaykou@gmail.com>
2024-11-19 23:15:42 +0100

    Fixing bugs into Pyspark and GCS configs

--- pyspark/data_load.py
+++ pyspark/data_load.py
@@ -5,11 +5,14 @@ from pyspark.sql import SparkSession
 
 load_dotenv(dotenv_path=".secrets")
 
-gcs_key_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-
 spark = SparkSession.builder \
     .appName("GCS Read CO2 data") \
-    .config("spark.jars", "../configs/gcs-connector-hadoop3-latest.jar") \
+    .config("spark.jars", "configs/gcs-connector-hadoop3-latest.jar") \
+    .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem") \
     .config("spark.hadoop.google.cloud.auth.service.account.enable", "true") \
-    .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", gcs_key_path) \
+    .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", os.getenv("GOOGLE_APPLICATION_CREDENTIALS")) \
     .getOrCreate()
+    
+
+df = spark.read.format("csv").option("header", "true").load("gs://co2-data-bucket/data/annual-co2-emissions-per-country.csv")
+df.show()
